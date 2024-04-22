752b15be5690957f744d8bc0ae110eff942a218c
mvstermind <zablockidawid1@gmail.com>
2024-04-22 18:46:08 +0200

    idk what's happening but it's doing something?

--- main.go
+++ main.go
@@ -1,16 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/nsf/termbox-go"
+)
 
 func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
 
-	s := []string{"test ", "word ", "less go "}
+	var s []string = []string{
+		"test ",
+		"halo ",
+	}
+	var c []byte
 
-	// get single character from array
+	highlightStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("205")).Bold(true)
+
 	for _, v := range s {
 		for i := 0; i < len(v); i++ {
-			fmt.Printf("%c\n", v[i])
+			c = append(c, v[i])
 		}
+	}
 
+	fmt.Println(s)
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			for _, char := range c {
+
+				if ev.Ch == rune(char) {
+					print(highlightStyle.Render(string(ev.Ch)))
+				}
+
+				if ev.Ch == '~' {
+					os.Exit(1)
+				}
+			}
+		case termbox.EventError:
+			panic(ev.Err)
+
+		}
 	}
 }
