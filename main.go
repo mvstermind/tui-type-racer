ec622ba8bb03c8fa77dba92b2298b1deee70ab65
mvstermind <zablockidawid1@gmail.com>
2024-04-22 20:00:40 +0200

    this will suck

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	// gosh scrapping this one for now, will come back when i'll get better at coding
+	sentence := "this is the test sentence"
+
+	fmt.Printf("%s\n", sentence)
+	//	lenSen := len(sentence)
+
+	b := bufio.NewReader(os.Stdin)
+	input, err := b.ReadString('\n')
+	if err != nil {
+		return
+	}
+	re, err := compInputs(sentence, input)
+
+	fmt.Println(re)
+}
+
+func compInputs(o string, u string) (string, error) {
+
+	var oirignal int
+	for _, v := range o {
+
+	}
+	return "", nil
+
 }
